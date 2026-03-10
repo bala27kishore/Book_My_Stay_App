2a4d94c3d6ff3def79ba8fb7d0effe9d6417ae41
bala27kishore <bala27kishore@gmail.com>
2026-03-10 14:39:58 +0530

    UC1: Add welcome message module

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class BookMyStayApp {
+
+    /**
+     * Application entry point.
+     *
+     * This method is the first method executed
+     * when the program is launched by the JVM.
+     *
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("  Welcome to Hotel Booking System");
+        System.out.println("=================================");
+        System.out.println("System started successfully.");
+
+    }
+}
